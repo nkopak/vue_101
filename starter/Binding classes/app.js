1904751ce203b644eca7c0a8534e83f0bdbd4bdd
nkopak <nkopak@leobit.com>
2023-05-18 17:32:35 +0300

    Binding styles, conditional rendering

--- starter/Binding classes/app.js
+++ starter/Binding classes/app.js
@@ -2,12 +2,20 @@ let vm = Vue.createApp({
     data() {
         return {
             isPurple: false,
-            selectedColor: ''
+            selectedColor: '',
+            size: 150,
+            mode: 1
         }
     },
     computed: {
         circle_classes() {
             return { purple: this.isPurple }
+        },
+        circle_size() {
+            return { width: this.size + 'px', height: this.size + 'px', lineHeight: this.size + 'px' }
+        },
+        circle_rotation() {
+            return {transform: `rotate(30deg)`}
         }
     }
 }).mount('#app')
