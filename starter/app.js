4db22f93a4db319fdfe72fb766149ea18f5f3460
nkopak <nkopak@leobit.com>
2023-05-18 10:39:08 +0300

    Methods

--- starter/app.js
+++ starter/app.js
@@ -1,11 +1,20 @@
-Vue.createApp({
+const vm = Vue.createApp({
     data(){
         return {
             firstName: 'Nazar',
             lastName: 'Dev'
         }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName}`
+        }
     }
 }).mount('#app')
+
+setTimeout(()=>{
+    vm.firstName = 'Bob'
+}, 2000)
 
 Vue.createApp({
     data(){
