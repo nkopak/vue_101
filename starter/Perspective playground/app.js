edb9e30e38296674e334fc223f58498c92aa4369
nkopak <nkopak@leobit.com>
2023-05-22 17:44:38 +0300

    Copiyng to clipboard

--- starter/Perspective playground/app.js
+++ starter/Perspective playground/app.js
@@ -25,6 +25,12 @@ Vue.createApp({
             this.rotateX = 0
             this.rotateY = 0
             this.rotateZ = 0
+        },
+        async copy() {
+            let text = `transform:${this.box.transform};`
+            await navigator.clipboard.writeText(text)
+
+            alert('CSS copied to clipboard')
         }
     }
 }).mount('#app')
