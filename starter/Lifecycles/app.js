37121d5410a9086f4e702d8dfe8ae5b6cfb3ac1f
nkopak <nkopak@leobit.com>
2023-05-22 18:26:14 +0300

    Using lifecycle hooks

--- starter/Lifecycles/app.js
+++ starter/Lifecycles/app.js
@@ -3,6 +3,31 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+    console.log('Before create function called!', this.message)
+  },
+  created() {
+    console.log('Created function called!', this.message)
+  },
+  beforeMount() {
+    console.log('beforeMount function called!', this.$el)
+    // $el - element Vue is mounted to
+  },
+  mounted() {
+    console.log('Mounted function called!', this.$el)
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate() called')
+  },
+  updated() {
+    console.log('Updated() called')
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount() called')
+  },
+  unmounted() {
+    console.log('unmounted() called')
   }
 })
 
