ad8855c98b35bf5da9abfafb55512c41e09cc7f2
nkopak <nkopak@leobit.com>
2023-05-24 16:54:12 +0300

    Vue components

--- starter/Vue Compiler/app.js
+++ starter/Vue Compiler/app.js
@@ -1,25 +1,33 @@
 let vm = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!"
-    }
-  },
-  template: `{{message}}`
+  // data() {
+  //   return {
+  //     message: "Hello world!"
+  //   }
+  // },
+  // template: `{{message}}`
 })
 
+vm.component('hello', {
+  template: `<h2>{{ message }}</h2>`,
+  data() {
+    return {
+      message: 'Hello Vue component!'
+    }
+  }
+})
 
 vm.mount('#app')
 
-let vm2 = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!"
-    }
-  },
-  render() {
-    return Vue.h(
-      'h1',
-      this.message
-    )
-  }
-}).mount('#app2')
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!"
+//     }
+//   },
+//   render() {
+//     return Vue.h(
+//       'h1',
+//       this.message
+//     )
+//   }
+// }).mount('#app2')
