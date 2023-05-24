17f8b2daf53e9243496eb728b0737a95173ea476
nkopak <nkopak@leobit.com>
2023-05-24 16:26:21 +0300

    The Vue compiler

---
+++ starter/Vue Compiler/app.js
@@ -0,0 +1,25 @@
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  },
+  template: `{{message}}`
+})
+
+
+vm.mount('#app')
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
+    }
+  },
+  render() {
+    return Vue.h(
+      'h1',
+      this.message
+    )
+  }
+}).mount('#app2')
