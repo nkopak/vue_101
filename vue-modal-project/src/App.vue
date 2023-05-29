eb612645cf9bfe612c1e4ba6446f65abd9ee8d17
nkopak <nkopak@leobit.com>
2023-05-29 16:58:18 +0300

    project setup, template refs

---
+++ vue-modal-project/src/App.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click here</button>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'My first Vue app',
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name.value);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
