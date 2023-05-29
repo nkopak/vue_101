fcd4eb195bceba28bd4fce6b565cc9c68650828d
nkopak <nkopak@leobit.com>
2023-05-29 18:17:25 +0300

    Props, global/scoped css

---
+++ vue-modal-project/src/components/Modal.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="backdrop">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <h2>{{ header }}</h2>
+            <p>Modal content</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'modal-element',
+  props: ['header', 'theme'],
+};
+</script>
+
+<style scoped>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    .modal.sale {
+        background-color: crimson;
+        color: wheat;
+    }
+</style>
