f157693f0fef7936392fa2731db1c4d9a1832361
nkopak <nkopak@leobit.com>
2023-05-29 10:48:51 +0300

    Slots

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,21 +2,51 @@
   <h3>Hey</h3>
     <greeting :age="age"></greeting>
     <user :age="age" @age-change="updateAge" :ageChangeFn="updateAgeCB"></user>
+    <app-form>
+      <template v-slot:help>
+        <p>{{ help }}</p>
+      </template>
+      <template v-slot:fields>
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="username">
+        <input type="password" placeholder="password">
+      </template>
+      <template v-slot:buttons>
+        <button type="submit">Submit</button>
+      </template>
+      <p>Dummy text</p>
+    </app-form>
+    <hr/>
+    <app-form>
+      <template v-slot:help>
+        <p>Contact text.</p>
+      </template>
+      <template v-slot:fields>
+        <input type="text" placeholder="email">
+        <input type="text" placeholder="message">
+      </template>
+      <template v-slot:buttons>
+        <button type="submit">Submit</button>
+      </template>
+    </app-form>
 </template>
 
 <script>
   import Greeting from '@/components/Greeting.vue' //Registering component locally
   import User from '@/components/User.vue'
+  import AppForm from "@/components/Form.vue"
 
    export default {
     name: 'App',
     components: {
       Greeting,
-      User
+      User,
+      AppForm
     },
     data() {
       return {
-        age:15
+        age:15,
+        help: 'This is HELP!'
       }
     },
     methods: {
---
+++ vue-project/src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+   <form>
+      <slot>No form to render</slot>
+      <div class="help">
+        <slot name="help"></slot>
+      </div>
+      <div class="fields">
+        <slot name="fields"></slot>
+      </div>
+      <slot></slot>
+      <div class="buttons">
+        <slot name="buttons"></slot>
+      </div>
+    </form> 
+</template>
