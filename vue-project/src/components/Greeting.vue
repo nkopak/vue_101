43e90f7557e8865a625e17aa337eee53d6d04c8f
nkopak <nkopak@leobit.com>
2023-05-29 10:09:56 +0300

    Creat vue app

---
+++ vue-project/src/components/Greeting.vue
@@ -0,0 +1,23 @@
+<template>
+    <p v-if="age > 18">{{ msg }}</p>
+    <p v-else>You must be 18 years old to see this</p>
+</template>
+
+<script>
+    export default {
+        name: 'Greeting',
+            data() {
+                return {
+                    msg: 'Hello vueJS'
+                }
+        },
+        props: ["age"]
+  }
+</script>
+
+<style scoped lang="scss">
+/* Scoped styles will only apply to current component */
+    p:hover {
+        color: darken(#cc4444, 15%);
+    }
+</style>
