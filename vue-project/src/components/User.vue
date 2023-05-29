8cf6c843a36d00de3077d39cba3faafa1ba81e94
nkopak <nkopak@leobit.com>
2023-05-29 10:09:56 +0300

    Creat vue app

---
+++ vue-project/src/components/User.vue
@@ -0,0 +1,38 @@
+<template>
+    <button type="button" @click="onClickAge">Increase age</button>
+    <button type="button" @click="ageChangeFn(3)">Increase age CB</button>
+    <p>
+        The user is {{ age }} years old
+    </p>
+    <p>
+        Doubdle age: {{ ageDoubled }}
+    </p>
+</template>
+
+<script>
+ export default {
+    name: 'User',
+    props: {
+        age: {
+            type: Number,
+            required: true
+        },
+        ageChangeFn: Function
+    },
+    emits: ['age-change'],
+    methods: {
+        onClickAge() {
+            this.$emit('age-change', 3)
+        }
+    },
+    computed: {
+        ageDoubled() {
+            return this.age * 2
+        }
+    }
+ }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
