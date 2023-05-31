979101c883c350597df23a1c7a8a100082b26f82
nkopak <nkopak@leobit.com>
2023-05-31 12:15:58 +0300

    Routing

---
+++ vue-router-jobs/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate totam ut soluta eaque distinctio natus veniam id quae earum quos in ratione voluptatibus nemo obcaecati ea, suscipit neque labore! Fugit sapiente enim iusto aut facilis modi reiciendis atque nesciunt quibusdam odit incidunt quisquam doloremque quidem repudiandae, vel officia, culpa suscipit at ea? Iusto nihil, et aliquid deleniti maiores distinctio error voluptatem! Ex, dignissimos quos accusamus aliquid sint fugit quo velit!</p>
+  </div>
+</template>
---
+++ vue-router-jobs/src/views/jobs/JobsView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h2>Jobs</h2>
+        <div v-for="job in jobs" :key="job.id" class="job">
+            <router-link :to="{name: 'jobDetails', params: {id: job.id}}">
+                <h2>{{ job.title }}</h2>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {title: 'Node', id: 1, details: 'Some stuff'},
+                {title: '.NET', id: 2, details: 'More stuff'},
+                {title: 'Java', id: 3, details: 'Awesome stuff'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+.job h2{
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+  }
+  .job h2:hover {
+    background: #ddd;
+  }
+  .job a{
+    text-decoration: none;
+  }
+
+</style>
