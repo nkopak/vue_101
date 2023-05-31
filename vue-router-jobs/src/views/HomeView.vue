abeed4a22687ded79e3a9176c7548362aafd01dd
nkopak <nkopak@leobit.com>
2023-05-31 12:15:58 +0300

    Routing

---
+++ vue-router-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Home page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, exercitationem? Modi quod in debitis! Earum consequatur, adipisci enim at perferendis exercitationem est tempore consectetur atque magni maxime iste inventore, hic commodi eaque corporis id porro, similique sapiente! Maiores a quam ex quos praesentium minima ea, repudiandae nihil aut iusto hic.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis voluptas dicta numquam, necessitatibus, voluptate nemo possimus, obcaecati totam delectus aliquam iste sunt magni ipsum neque unde autem nobis consequuntur perferendis.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: { }
+}
+</script>
