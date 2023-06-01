f480e85ca69b53b53e039d66332043063a73118d
nkopak <nkopak@leobit.com>
2023-06-01 16:23:11 +0300

    Vue, tailwind added

---
+++ vue-geocoding-app/client/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1 class="text-xl text-red-500">Testing tailwind</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
