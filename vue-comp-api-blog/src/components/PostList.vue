0c2d299910a6cd0a65b3496230d1fa7f2b48ec17
nkopak <nkopak@leobit.com>
2023-06-01 11:33:35 +0300

    Composition api blog

---
+++ vue-comp-api-blog/src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+// import { onMounted, onUnmounted, onUpdated } from "vue";
+import SinglePost from "./SinglePost.vue";
+
+export default {
+  props: ["posts"],
+  components: {
+    SinglePost,
+  },
+  setup() {
+    // onMounted();
+    // onUnmounted();
+    // onUpdated();
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-comp-api-blog/src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import DetailsView from "../views/DetailsView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/posts/:id",
+    name: "details",
+    component: DetailsView,
+    props: true,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
